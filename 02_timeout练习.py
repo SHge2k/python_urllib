188a4838258b60d9aa02ce53e7db443d0c26add6
常geek <1539665424@qq.com>
2022-02-18 19:30:19 +0800

    2.18第二次提交

--- 02_timeout练习.py
+++ 02_timeout练习.py
@@ -1,4 +1,21 @@
-from socket import timeout
+# import socket
+# import urllib.request
+"""
+设置时间超时,抛出错误
+
+"""
+# response = urllib.request.urlopen('http://www.httpbin.org/get',timeout=0.1)
+# print(response.read())
+
+import socket
 import urllib.request
-response = urllib.request.urlopen('http://www.httpbin.org/get',timeout=0.1)
-print(response.read())
+import urllib.error
+
+try:
+    response = urllib.request.urlopen('https://www.httpbin.org/get',timeout=0.1)
+    print(response)
+    
+except urllib.error.URLError as e:
+    if isinstance(e.errno,socket.timeout):
+        print("Time Out") 
+    
